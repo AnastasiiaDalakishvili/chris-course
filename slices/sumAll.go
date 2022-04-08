e8185d9c4b0cd6ab8b0fcdab2193c8e2a4a12610
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-04-08 22:17:21 +0100

    PASS test: sumAll function with slices

---
+++ slices/sumAll.go
@@ -0,0 +1,11 @@
+package slices
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		tail := numbers[1:]
+		sums = append(sums, Sum(tail))
+	}
+
+	return sums
+}
--- slices/sumAll_test.go
+++ slices/sumAll_test.go
@@ -1,12 +1,15 @@
 package slices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestSumAll(t *testing.T) {
-	got := SumAll([]int{1, 2}, []int{0, 9})
-	want := []int{3, 9}
+func TestSumAllTails(t *testing.T) {
+	got := SumAllTails([]int{1, 2}, []int{0, 9})
+	want := []int{2, 9}
 
-	if got != want {
+	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
