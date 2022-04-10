7a97553f33fa7b9ba888c55bab4b1185018abb7d
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-04-10 11:54:30 +0100

    PASS test: update the definition word funcD

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -68,6 +68,28 @@ func testUpdate(t *testing.T) {
 	dictionary.Update(word, newDefinition)
 
 	assertDefinition(t, dictionary, word, newDefinition)
+
+	t.Run("existing word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		newDefinition := "new definition"
+		dictionary := Dictionary{word: definition}
+
+		err := dictionary.Update(word, newDefinition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, newDefinition)
+	})
+
+	t.Run("new word", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+		dictionary := Dictionary{}
+
+		err := dictionary.Update(word, definition)
+
+		assertError(t, err, ErrWordDoesNotExist)
+	})
 }
 
 func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
