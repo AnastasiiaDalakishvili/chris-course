e0221fe9be75d588a78868d3cf15e8a297e7620b
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-04-16 14:47:39 +0100

    dependency injection

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -92,18 +92,6 @@ func testUpdate(t *testing.T) {
 	})
 }
 
-func TestDelete(t *testing.T) {
-	word := "test"
-	dictionary := Dictionary{word: "test definition"}
-
-	dictionary.Delete(word)
-
-	_, err := dictionary.Search(word)
-	if err != ErrNotFound {
-		t.Errorf("Expected %q to be deleted", word)
-	}
-}
-
 func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
 	t.Helper()
 
