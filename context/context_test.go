7d021eaec800cb75066f226434d7292961e3650d
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-04-29 08:37:42 +0100

    add fucntionality to give up if request is canceled

--- context/context_test.go
+++ context/context_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -39,6 +40,26 @@ func TestServer(t *testing.T) {
 
 		if store.cancelled {
 			t.Error("it should not have cancelled the store")
+		}
+	})
+
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		data := "hello, world"
+		store := &SpyStore{response: data}
+		svr := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+
+		response := httptest.NewRecorder()
+
+		svr.ServeHTTP(response, request)
+
+		if !store.cancelled {
+			t.Error("store was not told to cancel")
 		}
 	})
 }
